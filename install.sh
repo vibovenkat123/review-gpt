7cd0c859282519a366e7ff7dcf0612ef9c92a18d
vibovenkat123 <vibovenkat@gmail.com>
2023-03-04 17:12:57 -0600

    fix: make url variable name descriptive

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ else
     escape() { :; }
 fi
 
-stable_download_url="https://github.com/vibovenkat123/review-gpt/releases/download/latest"
+latest_download_url="https://github.com/vibovenkat123/review-gpt/releases/download/latest"
 declare -a binaries=("rgpt-linux-mips" "rgpt-linux-mips64" "rgpt-linux-mips64le" "rgpt-linux-mipsle" "rgpt-linux-ppc64" "rgpt-linux-ppc64le" "rgpt-linux-386" "rgpt-linux-amd64" "rgpt-linux-arm" "rgpt-linux-arm64" "rgpt-macos-arm64" "rgpt-macos-amd64" "rgpt-windows-386" "rgpt-windows-amd64" "rgpt-windows-arm64")
 mkcolor() { escape "1;$1"; }
 mksecondarycolor() { escape "0;$1"; }
@@ -91,7 +91,7 @@ download_binaries() {
     arrow "Going to bin directory"
     cd /usr/local/bin
     arrow "Downloading binaries"
-    sudo curl -LJO $stable_download_url/$binary_name
+    sudo curl -LJO $latest_download_url/$binary_name
     arrow "Moving the binary to the correct name"
     sudo mv $binary_name rgpt
     arrow "Giving the file executing permissions"
